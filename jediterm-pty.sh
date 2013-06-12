7c70adc15bfe1759c8752334fd62f501867c5832
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-06-12 15:56:04 +0200

    sh file to start terminal

---
+++ jediterm-pty.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp "lib/JPty.jar:lib/guava-12.0.jar:lib/jna.jar:lib/jsch-0.1.50.jar:lib/junit-4.10.jar:lib/jzlib-1.07.jar:lib/log4j-1.2.14.jar:lib/purejavacomm-0.0.16.jar:build/jediterm-pty-0.04.jar" com.jediterm.pty.PtyMain 

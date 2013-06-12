40c199582f10308f60cb2ea7b76a528ad414913a
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-06-12 20:11:58 +0200

    Dont update buffer panel if text hasn't changed.

--- src-emulator/com/jediterm/emulator/debug/BufferPanel.java
+++ src-emulator/com/jediterm/emulator/debug/BufferPanel.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.jediterm.emulator.debug;
 
@@ -14,39 +14,43 @@ import java.awt.event.ItemListener;
 
 
 public class BufferPanel extends JPanel {
-	public BufferPanel(final SwingJediTerminal terminal){
-		super(new BorderLayout());
-		final JTextArea area = new JTextArea();
-		add(area, BorderLayout.NORTH);
-		
-		final DebugBufferType[] choices = DebugBufferType.values();
-		
-		final JComboBox chooser = new JComboBox(choices);
-		add(chooser, BorderLayout.NORTH);
-		
-		area.setFont(Font.decode("Monospaced-14"));
-		add(new JScrollPane(area), BorderLayout.CENTER);
-		
-		class Updater implements ActionListener, ItemListener{
-			void update(){
-				final DebugBufferType type = (DebugBufferType) chooser.getSelectedItem();
-				final String text = terminal.getBufferText(type);
-				area.setText(text);
-			}
-			
-			public void actionPerformed(final ActionEvent e) {
-				update();
-			}
+  public BufferPanel(final SwingJediTerminal terminal) {
+    super(new BorderLayout());
+    final JTextArea area = new JTextArea();
+    add(area, BorderLayout.NORTH);
 
-			public void itemStateChanged(final ItemEvent e) {
-				update();
-			}
-		}
-		final Updater up = new Updater();
-		chooser.addItemListener(up);
-		final Timer timer = new Timer(1000, up);
-		timer.setRepeats(true);
-		timer.start();
-		
-	}
+    final DebugBufferType[] choices = DebugBufferType.values();
+
+    final JComboBox chooser = new JComboBox(choices);
+    add(chooser, BorderLayout.NORTH);
+
+    area.setFont(Font.decode("Monospaced-14"));
+    add(new JScrollPane(area), BorderLayout.CENTER);
+
+    class Updater implements ActionListener, ItemListener {
+      private String myLastUpdate = "";
+
+      void update() {
+        final DebugBufferType type = (DebugBufferType)chooser.getSelectedItem();
+        final String text = terminal.getBufferText(type);
+        if (!text.equals(myLastUpdate)) {
+          area.setText(text);
+          myLastUpdate = text;
+        }
+      }
+
+      public void actionPerformed(final ActionEvent e) {
+        update();
+      }
+
+      public void itemStateChanged(final ItemEvent e) {
+        update();
+      }
+    }
+    final Updater up = new Updater();
+    chooser.addItemListener(up);
+    final Timer timer = new Timer(1000, up);
+    timer.setRepeats(true);
+    timer.start();
+  }
 }

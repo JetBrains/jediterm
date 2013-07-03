8357f497599bc9a51653c67a3c68b6dc4ae96eb2
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-07-03 16:32:45 +0200

    Ignore abstract emulator test.

--- tests/com/jediterm/AbstractEmulatorTest.java
+++ tests/com/jediterm/AbstractEmulatorTest.java
@@ -11,6 +11,7 @@ import com.jediterm.util.BackBufferTerminal;
 import com.jediterm.util.FileUtil;
 import com.jediterm.util.NullTerminalOutputStream;
 import junit.framework.TestCase;
+import org.junit.Ignore;
 
 import java.awt.*;
 import java.io.File;
@@ -19,6 +20,7 @@ import java.io.IOException;
 /**
  * @author traff
  */
+@Ignore
 public abstract class AbstractEmulatorTest extends TestCase {
   protected static void assertColor(TextStyle style, Color foreground, Color background) {
     assertEquals(foreground, style.getForeground());

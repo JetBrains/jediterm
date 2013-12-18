38c2ab0435cb5fb1247f66251fc4a3c72687a27f
Plonk42 <plonk42+github@gmail.com>
2013-12-18 14:36:23 +0100

    Fix previous commit : use RootLogger

--- src-terminal/com/jediterm/terminal/ui/AbstractTerminalFrame.java
+++ src-terminal/com/jediterm/terminal/ui/AbstractTerminalFrame.java
@@ -93,7 +93,7 @@ public abstract class AbstractTerminalFrame {
       logLevel.add(new AbstractAction(l.toString()) {
         @Override
         public void actionPerformed(ActionEvent e) {
-          LOG.setLevel(l);
+          Logger.getRootLogger().setLevel(l);
         }
       });
     }

f790cf650ffb25acda04d6b316bd5541d46511a3
Plonk42 <plonk42+github@gmail.com>
2014-07-18 16:44:50 +0200

    Update lib version in jediterm-pty.sh

--- jediterm-pty.sh
+++ jediterm-pty.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "lib/JPty.jar:lib/guava-12.0.jar:lib/jna.jar:lib/jsch-0.1.50.jar:lib/junit-4.10.jar:lib/jzlib-1.07.jar:lib/log4j-1.2.14.jar:lib/purejavacomm-0.0.16.jar:build/jediterm-pty-0.04.jar" com.jediterm.pty.PtyMain 
+java -cp "lib/pty4j-0.3.jar:lib/guava-14.0.1.jar:lib/jna.jar:lib/jsch-0.1.50.jar:lib/junit-4.10.jar:lib/jzlib-1.1.1.jar:lib/log4j-1.2.14.jar:lib/purejavacomm-0.0.17.jar:build/jediterm-pty-2.0.jar" com.jediterm.pty.PtyMain 

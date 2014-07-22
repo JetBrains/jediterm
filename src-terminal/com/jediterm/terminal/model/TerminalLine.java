JetBrains/jediterm

commit d62c023bd75a1f55a69cc7a5e843178f5f2c3d8b

Apply style when needed even in empty areas

src-terminal/com/jediterm/terminal/model/TerminalLine.java
@@ -44,6 +44,11 @@ public class TerminalLine {
     return sb.toString();
   }
 
+  // return the position of the first NUL char (ie. the end of actual text)
+  public int getNulIndex() {
+    return myTextEntries.getNulIndex();
+  }
+
   public boolean isWrapped() {
     return myWrapped;
   }
@@ -52,8 +57,9 @@ public class TerminalLine {
     myWrapped = wrapped;
   }
 
-  public void clear() {
+  public void clear(@NotNull TextEntry filler) {
     myTextEntries.clear();
+    myTextEntries.add(filler);
     setWrapped(false);
   }
 
@@ -122,17 +128,23 @@ public class TerminalLine {
   }
 
   public void deleteCharacters(int x) {
-    deleteCharacters(x, myTextEntries.length() - x);
+    deleteCharacters(x, TextStyle.EMPTY);
+  }
+
+  public void deleteCharacters(int x, @NotNull TextStyle style) {
+    deleteCharacters(x, myTextEntries.length() - x, style);
     // delete to the end of line : line is no more wrapped
     setWrapped(false);
   }
 
-  public void deleteCharacters(int x, int count) {
+  public void deleteCharacters(int x, int count, @NotNull TextStyle style) {
     int p = 0;
     TextEntries newEntries = new TextEntries();
 
+    int remaining = count;
+
     for (TextEntry entry : myTextEntries) {
-      if (count == 0) {
+      if (remaining == 0) {
         newEntries.add(entry);
         continue;
       }
@@ -148,20 +160,23 @@ public class TerminalLine {
         newEntries.add(new TextEntry(entry.getStyle(), entry.getText().subBuffer(0, dx)));
         p = x;
       }
-      if (dx + count < len) {
+      if (dx + remaining < len) {
         //part that left after deleting count 
         newEntries.add(new TextEntry(entry.getStyle(), entry.getText().subBuffer(dx + count, len - (dx + count))));
-        count = 0;
+        remaining = 0;
       } else {
-        count -= (len - dx);
+        remaining -= (len - dx);
         p = x;
       }
+    }
+    if (count > 0 && style != TextStyle.EMPTY) { // apply style to the end of the line
+      newEntries.add(new TextEntry(style, new CharBuffer(CharacterUtils.NUL_CHAR, count)));
     }
 
     myTextEntries = newEntries;
   }
 
-  public void insertBlankCharacters(int x, int count, int maxLen) {
+  public void insertBlankCharacters(int x, int count, int maxLen, @NotNull TextStyle style) {
     int len = myTextEntries.length();
     len = Math.min(len + count, maxLen);
 
@@ -172,9 +187,9 @@ public class TerminalLine {
     for (TextEntry entry : myTextEntries) {
       for (int i = 0; i < entry.getLength() && p < len; i++) {
         if (p == x) {
-          for (int j = 0; j < count; j++) {
+          for (int j = 0; j < count && p < len; j++) {
             buf[p] = CharacterUtils.EMPTY_CHAR;
-            styles[p] = TextStyle.EMPTY;
+            styles[p] = style;
             p++;
           }
         }
@@ -187,6 +202,18 @@ public class TerminalLine {
       if (p >= len) {
         break;
       }
+    }
+
+    // if not inserted yet (ie. x > len)
+    for (; p < x && p < len; p++) {
+      buf[p] = CharacterUtils.EMPTY_CHAR;
+      styles[p] = TextStyle.EMPTY;
+      p++;
+    }
+    for (; p < x + count && p < len; p++) {
+      buf[p] = CharacterUtils.EMPTY_CHAR;
+      styles[p] = style;
+      p++;
     }
 
     myTextEntries = collectFromBuffer(buf, styles);
@@ -214,11 +241,12 @@ public class TerminalLine {
 
   public void process(int y, StyledTextConsumer consumer, int startRow) {
     int x = 0;
+    int nulIndex = getNulIndex();
     for (TextEntry te: Lists.newArrayList(myTextEntries)) {
-      consumer.consume(x, y, te.getStyle(), te.getText(), startRow);
+      consumer.consume(x, y, nulIndex, te.getStyle(), te.getText(), startRow);
       x += te.getLength();
     }
-    consumer.consumeQueue(x, y, startRow);
+    consumer.consumeQueue(x, y, nulIndex, startRow);
   }
 
   static class TextEntry {
@@ -247,8 +275,10 @@ public class TerminalLine {
     private ArrayList<TextEntry> myTextEntries = new ArrayList<TextEntry>();
 
     private int myLength = 0;
+    private int myNulIndex = 0;
 
     public void add(TextEntry entry) {
+      myLength += entry.getLength();
       // NUL can only be at the end of the line
       if (!entry.getText().isNul()) {
         for (TextEntry t : myTextEntries) {
@@ -256,9 +286,9 @@ public class TerminalLine {
             t.getText().unNullify();
           }
         }
+        myNulIndex = myLength;
       }
       myTextEntries.add(entry);
-      myLength += entry.getLength();
     }
 
     private Collection<TextEntry> entries() {
@@ -273,9 +303,14 @@ public class TerminalLine {
       return myLength;
     }
 
+    public int getNulIndex() {
+      return myNulIndex;
+    }
+
     public void clear() {
       myTextEntries.clear();
       myLength = 0;
+      myNulIndex = 0;
     }
   }
 }

src-terminal/com/jediterm/terminal/StyledTextConsumer.java
@@ -13,13 +13,14 @@ public interface StyledTextConsumer {
    *
    * @param x indicates starting column of the characters
    * @param y indicates row of the characters
+   * @param nulIndex indicates position of the first NUL char (ie. the end of actual text)
    * @param style style of characters
    * @param characters text characters
    * @param startRow number of the first row.
    *                 It can be different for different buffers, e.g. backBuffer starts from 0, textBuffer and scrollBuffer from -count
    */
-  void consume(int x, int y, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow);
+  void consume(int x, int y, int nulIndex, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow);
 
-  void consumeQueue(int x, int y, int startRow);
+  void consumeQueue(int x, int y, int nulIndex, int startRow);
 
 }

src-terminal/com/jediterm/terminal/model/LinesBuffer.java
@@ -3,6 +3,7 @@ package com.jediterm.terminal.model;
 import com.google.common.collect.Lists;
 import com.jediterm.terminal.StyledTextConsumer;
 import com.jediterm.terminal.TextStyle;
+import com.jediterm.terminal.model.TerminalLine.TextEntry;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
@@ -71,7 +72,7 @@ public class LinesBuffer {
     return line.getText();
   }
 
-  public synchronized void insertLines(int y, int count, int lastLine) {
+  public synchronized void insertLines(int y, int count, int lastLine, @NotNull TextEntry filler) {
     LinesBuffer tail = new LinesBuffer();
 
     if (lastLine < getLineCount() - 1) {
@@ -84,7 +85,7 @@ public class LinesBuffer {
     }
 
     for (int i = 0; i < count; i++) {
-      head.addNewLine(TextStyle.EMPTY, CharBuffer.EMPTY);
+      head.addNewLine(filler);
     }
 
     head.moveBottomLinesTo(head.getLineCount(), this);
@@ -94,7 +95,7 @@ public class LinesBuffer {
     tail.moveTopLinesTo(tail.getLineCount(), this);
   }
 
-  public synchronized LinesBuffer deleteLines(int y, int count, int lastLine) {
+  public synchronized LinesBuffer deleteLines(int y, int count, int lastLine, @NotNull TextEntry filler) {
     LinesBuffer tail = new LinesBuffer();
 
     if (lastLine < getLineCount() - 1) {
@@ -114,7 +115,7 @@ public class LinesBuffer {
     head.moveBottomLinesTo(head.getLineCount(), this);
 
     for (int i = 0; i < toRemove; i++) {
-      addNewLine(TextStyle.EMPTY, CharBuffer.EMPTY);
+      addNewLine(filler);
     }
 
     tail.moveTopLinesTo(tail.getLineCount(), this);
@@ -128,15 +129,9 @@ public class LinesBuffer {
     line.writeString(x, str, style);
   }
 
-  public synchronized void clearLines(int startRow, int endRow) {
+  public synchronized void clearLines(int startRow, int endRow, @NotNull TextEntry filler) {
     for (int i = startRow; i <= endRow; i++) {
-      if (i < myLines.size()) {
-        TerminalLine line = myLines.get(i);
-        line.clear();
-      }
-      else {
-        break;
-      }
+      getLine(i).clear(filler);
     }
   }
 
@@ -144,14 +139,14 @@ public class LinesBuffer {
     myLines.clear();
   }
 
-  public synchronized void deleteCharacters(int x, int y, int count) {
+  public synchronized void deleteCharacters(int x, int y, int count, @NotNull TextStyle style) {
     TerminalLine line = getLine(y);
-    line.deleteCharacters(x, count);
+    line.deleteCharacters(x, count, style);
   }
 
-  public synchronized void insertBlankCharacters(final int x, final int y, final int count, final int maxLen) {
+  public synchronized void insertBlankCharacters(final int x, final int y, final int count, final int maxLen, @NotNull TextStyle style) {
     TerminalLine line = getLine(y);
-    line.insertBlankCharacters(x, count, maxLen);
+    line.insertBlankCharacters(x, count, maxLen, style);
   }
 
   public synchronized void clearArea(int leftX, int topY, int rightX, int bottomY, @NotNull TextStyle style) {

src-terminal/com/jediterm/terminal/model/TerminalTextBuffer.java
@@ -6,6 +6,7 @@ import com.jediterm.terminal.CharacterUtils;
 import com.jediterm.terminal.RequestOrigin;
 import com.jediterm.terminal.StyledTextConsumer;
 import com.jediterm.terminal.TextStyle;
+import com.jediterm.terminal.model.TerminalLine.TextEntry;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -122,6 +123,10 @@ public class TerminalTextBuffer {
     return myStyleState.getCurrent().createEmptyWithColors();
   }
 
+  private TextEntry createFillerEntry() {
+    return new TextEntry(createEmptyStyleWithCurrentColor(), new CharBuffer(CharacterUtils.NUL_CHAR, myWidth));
+  }
+
   public void deleteCharacters(final int x, final int y, final int count) {
     if (y > myHeight - 1 || y < 0) {
       LOG.error("attempt to delete in line " + y + "\n" +
@@ -131,13 +136,7 @@ public class TerminalTextBuffer {
     } else if (count == 0) { //nothing to do
       return;
     } else {
-      int to = y * myWidth + x;
-      int from = to + count;
-      int remain = myWidth - x - count;
-      LOG.debug("About to delete " + count + " chars on line " + y + ", starting from " + x +
-              " (from : " + from + " to : " + to + " remain : " + remain + ")");
-
-      myScreenBuffer.deleteCharacters(x, y, count);
+      myScreenBuffer.deleteCharacters(x, y, count, createEmptyStyleWithCurrentColor());
 
       fireModelChangeEvent();
     }
@@ -152,7 +151,7 @@ public class TerminalTextBuffer {
     } else if (count > 0) { //nothing to do
       int from = y * myWidth + x;
 
-      myScreenBuffer.insertBlankCharacters(x, y, count, myWidth);
+      myScreenBuffer.insertBlankCharacters(x, y, count, myWidth, createEmptyStyleWithCurrentColor());
 
       fireModelChangeEvent();
     }
@@ -214,7 +213,7 @@ public class TerminalTextBuffer {
         int count = 0;
 
         @Override
-        public void consume(int x, int y, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
+        public void consume(int x, int y, int nulIndex, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
           if (x == 0) {
             sb.append("\n");
           }
@@ -226,7 +225,7 @@ public class TerminalTextBuffer {
         }
 
         @Override
-        public void consumeQueue(int x, int y, int startRow) {
+        public void consumeQueue(int x, int y, int nulIndex, int startRow) {
           // no-op
         }
       });
@@ -350,23 +349,22 @@ public class TerminalTextBuffer {
   }
 
   public void insertLines(int y, int count, int scrollRegionBottom) {
-    myScreenBuffer.insertLines(y, count, scrollRegionBottom - 1);
+    myScreenBuffer.insertLines(y, count, scrollRegionBottom - 1, createFillerEntry());
 
     fireModelChangeEvent();
   }
 
   // returns deleted lines
   public LinesBuffer deleteLines(int y, int count, int scrollRegionBottom) {
-    LinesBuffer linesBuffer = myScreenBuffer.deleteLines(y, count, scrollRegionBottom - 1);
+    LinesBuffer linesBuffer = myScreenBuffer.deleteLines(y, count, scrollRegionBottom - 1, createFillerEntry());
     fireModelChangeEvent();
     return linesBuffer;
   }
 
   public void clearLines(int startRow, int endRow) {
-    myScreenBuffer.clearLines(startRow, endRow);
+    myScreenBuffer.clearLines(startRow, endRow, createFillerEntry());
     fireModelChangeEvent();
   }
-
 
   public void eraseCharacters(int leftX, int rightX, int y) {
     TextStyle style = createEmptyStyleWithCurrentColor();

src-terminal/com/jediterm/terminal/ui/TerminalPanel.java
@@ -561,16 +561,19 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Clipbo
       final int textWidth = myTerminalTextBuffer.getWidth();
       
       @Override
-      public void consume(int x, int y, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
+      public void consume(int x, int y, int nulIndex, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
         int row = y - startRow;
         drawCharacters(x, row, style, characters, gfx);
         if (mySelection != null) {
-          Pair<Integer, Integer> interval = mySelection.intersect(x, row + myClientScrollOrigin, characters.length());
-          if (interval != null) {
-            TextStyle selectionStyle = getSelectionStyle(style);
-            if (characters.isNul()) { // NUL area is non-breaking
+          TextStyle selectionStyle = getSelectionStyle(style);
+          if (characters.isNul()) { // NUL area is non-breaking
+            Pair<Integer, Integer> interval = mySelection.intersect(nulIndex, row + myClientScrollOrigin, textWidth - nulIndex);
+            if (interval != null) {
               drawCharacters(x, row, selectionStyle, characters, gfx);
-            } else {
+            }
+          } else {
+            Pair<Integer, Integer> interval = mySelection.intersect(x, row + myClientScrollOrigin, characters.length());
+            if (interval != null) {
               drawCharacters(interval.first, row, selectionStyle, characters.subBuffer(interval.first - x, interval.second), gfx);
             }
           }
@@ -578,14 +581,14 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Clipbo
       }
 
       @Override
-      public void consumeQueue(int x, int y, int startRow) {
+      public void consumeQueue(int x, int y, int nulIndex, int startRow) {
         // draw selection on the end of the line
+        if (x >= textWidth) {
+          return;
+        }
         int row = y - startRow;
-        if (mySelection != null) {
-          Pair<Point, Point> p = SelectionUtil.sortPoints(new Point(mySelection.getStart()), new Point(mySelection.getEnd()));
-          if ((row >= p.first.y && row < p.second.y) || (row == p.second.y && p.second.x >= x)) {
-            drawCharacters(x, row, getSelectionStyle(myStyleState.getCurrent()), new CharBuffer(CharacterUtils.EMPTY_CHAR, textWidth - x), gfx);
-          }
+        if (mySelection != null && mySelection.intersect(nulIndex, row + myClientScrollOrigin, textWidth - nulIndex) != null) {
+          drawCharacters(x, row, getSelectionStyle(TextStyle.EMPTY), new CharBuffer(CharacterUtils.EMPTY_CHAR, textWidth - x), gfx);
         }
       }
     });

tests/com/jediterm/TerminalTextBufferTest.java
@@ -40,12 +40,12 @@ public class TerminalTextBufferTest extends TestCase {
 
     terminalTextBuffer.processScreenLines(0, 10, new StyledTextConsumer() {
       @Override
-      public void consume(int x, int y, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
+      public void consume(int x, int y, int nulIndex, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
         assertNotNull(style);
       }
 
       @Override
-      public void consumeQueue(int x, int y, int startRow) {
+      public void consumeQueue(int x, int y, int nulIndex, int startRow) {
         // no-op
       }
     });

tests/com/jediterm/util/ArrayBasedTextConsumer.java
@@ -24,14 +24,14 @@ public class ArrayBasedTextConsumer implements StyledTextConsumer {
   }
 
   @Override
-  public void consume(int x, int y, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
+  public void consume(int x, int y, int nulIndex, @NotNull TextStyle style, @NotNull CharBuffer characters, int startRow) {
     for (int i = 0; i<characters.length(); i++) {
       myBuf[y - startRow][x + i] = characters.charAt(i);
     }
   }
 
   @Override
-  public void consumeQueue(int x, int y, int startRow) {
+  public void consumeQueue(int x, int y, int nulIndex, int startRow) {
     // no-op
   }
 

commit 157e92590f076c96488f3fcbfdfeca380bb731a4

Fix bug introduced in d62c023bd75a1f55a69cc7a5e843178f5f2c3d8b

src-terminal/com/jediterm/terminal/model/TerminalLine.java
@@ -162,7 +162,7 @@ public class TerminalLine {
       }
       if (dx + remaining < len) {
         //part that left after deleting count 
-        newEntries.add(new TextEntry(entry.getStyle(), entry.getText().subBuffer(dx + count, len - (dx + count))));
+        newEntries.add(new TextEntry(entry.getStyle(), entry.getText().subBuffer(dx + remaining, len - (dx + remaining))));
         remaining = 0;
       } else {
         remaining -= (len - dx);
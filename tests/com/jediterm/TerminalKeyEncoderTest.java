c081032d71cdc120385c1b44ae7f4ce6b0cc2752
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2016-03-21 17:42:56 +0100

    Test is not valid anymore

--- tests/com/jediterm/TerminalKeyEncoderTest.java
+++ tests/com/jediterm/TerminalKeyEncoderTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 
 import java.awt.event.InputEvent;
+import java.nio.charset.Charset;
 
 /**
  * @author traff
@@ -19,13 +20,5 @@ public class TerminalKeyEncoderTest extends TestCase {
   public void testAltLeft() {
     TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
     Assert.assertArrayEquals(new byte[]{TerminalKeyEncoder.ESC, TerminalKeyEncoder.ESC, '[', 'D'}, terminalKeyEncoder.getCode(37, InputEvent.ALT_MASK));
-  }
-
-  public void testAltLeft_AltNoEscape() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    terminalKeyEncoder.setAltSendsEscape(false);
-    
-    // we expect to have '3' (corresponds to alt) before the final char in left key code
-    Assert.assertArrayEquals(new byte[]{TerminalKeyEncoder.ESC, '[', '3', 'D'}, terminalKeyEncoder.getCode(37, InputEvent.ALT_MASK));
   }
 }

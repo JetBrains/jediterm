619188ca8f8ee8e393a1a0ce8e6a941159dd5cd5
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2016-11-10 15:52:33 +0100

    Fix test compilation

--- terminal/tests/src/com/jediterm/TerminalKeyEncoderTest.java
+++ terminal/tests/src/com/jediterm/TerminalKeyEncoderTest.java
@@ -1,5 +1,6 @@
 package com.jediterm;
 
+import com.google.common.base.Ascii;
 import com.jediterm.terminal.TerminalKeyEncoder;
 import junit.framework.TestCase;
 import org.junit.Assert;
@@ -14,11 +15,11 @@ public class TerminalKeyEncoderTest extends TestCase {
   
   public void testAltBackspace() {
     TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    Assert.assertArrayEquals(new byte[]{TerminalKeyEncoder.ESC, TerminalKeyEncoder.DEL}, terminalKeyEncoder.getCode('\b', InputEvent.ALT_MASK));
+    Assert.assertArrayEquals(new byte[]{Ascii.ESC, Ascii.DEL}, terminalKeyEncoder.getCode('\b', InputEvent.ALT_MASK));
   }
 
   public void testAltLeft() {
     TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    Assert.assertArrayEquals(new byte[]{TerminalKeyEncoder.ESC, TerminalKeyEncoder.ESC, '[', 'D'}, terminalKeyEncoder.getCode(37, InputEvent.ALT_MASK));
+    Assert.assertArrayEquals(new byte[]{Ascii.ESC, Ascii.ESC, '[', 'D'}, terminalKeyEncoder.getCode(37, InputEvent.ALT_MASK));
   }
 }

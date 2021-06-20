7e42fc1261ffd0b593557afa71851f1d1df76804
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2021-06-21 01:40:32 +0300

    add basic shell example

---
+++ JediTerm/src/main/java/com/jediterm/example/BasicTerminalShellExample.java
@@ -0,0 +1,57 @@
+package com.jediterm.example;
+
+import com.jediterm.pty.PtyProcessTtyConnector;
+import com.jediterm.terminal.TtyConnector;
+import com.jediterm.terminal.ui.JediTermWidget;
+import com.jediterm.terminal.ui.UIUtil;
+import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
+import com.pty4j.PtyProcess;
+import com.pty4j.PtyProcessBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicTerminalShellExample {
+
+  private static @NotNull JediTermWidget createTerminalWidget() {
+    JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
+    widget.setTtyConnector(createTtyConnector());
+    widget.start();
+    return widget;
+  }
+
+  private static @NotNull TtyConnector createTtyConnector() {
+    try {
+      Map<String, String> envs = System.getenv();
+      String[] command;
+      if (UIUtil.isWindows) {
+        command = new String[]{"cmd.exe"};
+      } else {
+        command = new String[]{"/bin/bash", "--login"};
+        envs = new HashMap<>(System.getenv());
+        envs.put("TERM", "xterm-256color");
+      }
+
+      PtyProcess process = new PtyProcessBuilder().setCommand(command).setEnvironment(envs).start();
+      return new PtyProcessTtyConnector(process, StandardCharsets.UTF_8);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private static void createAndShowGUI() {
+    JFrame frame = new JFrame("Basic Terminal Shell Example");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setContentPane(createTerminalWidget());
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    // Create and show this application's GUI in the event-dispatching thread.
+    SwingUtilities.invokeLater(BasicTerminalShellExample::createAndShowGUI);
+  }
+}

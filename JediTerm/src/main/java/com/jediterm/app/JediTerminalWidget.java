212f66654776c8ef592f852443d4fa1f145db3d0
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2022-12-13 22:10:19 +0100

    drop outdated JBScrollBar not working with recent jbr 17

--- JediTerm/src/main/java/com/jediterm/app/JediTerminalWidget.java
+++ JediTerm/src/main/java/com/jediterm/app/JediTerminalWidget.java
@@ -2,19 +2,11 @@ package com.jediterm.app;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.ui.components.JBScrollBar;
-import com.intellij.ui.components.JBScrollPane;
-import com.intellij.util.ui.RegionPainter;
-import com.jediterm.terminal.SubstringFinder;
 import com.jediterm.terminal.model.StyleState;
 import com.jediterm.terminal.model.TerminalTextBuffer;
 import com.jediterm.terminal.ui.JediTermWidget;
 import com.jediterm.terminal.ui.settings.SettingsProvider;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.Objects;
 
 public class JediTerminalWidget extends JediTermWidget implements Disposable {
 
@@ -34,28 +26,6 @@ public class JediTerminalWidget extends JediTermWidget implements Disposable {
     return panel;
   }
 
-
-  protected @NotNull JScrollBar createScrollBar() {
-    JBScrollBar bar = new JBScrollBar();
-    bar.putClientProperty(JBScrollPane.Alignment.class, JBScrollPane.Alignment.RIGHT);
-    bar.putClientProperty(JBScrollBar.TRACK, (RegionPainter<Object>) (g, x, y, width, height, object) -> {
-      SubstringFinder.FindResult result = myTerminalPanel.getFindResult();
-      if (result != null) {
-        int modelHeight = bar.getModel().getMaximum() - bar.getModel().getMinimum();
-        int anchorHeight = Math.max(2, height / modelHeight);
-
-        Color color = mySettingsProvider.getTerminalColorPalette()
-          .getBackground(Objects.requireNonNull(mySettingsProvider.getFoundPatternColor().getBackground()));
-        g.setColor(color);
-        for (SubstringFinder.FindResult.FindItem r : result.getItems()) {
-          int where = height * r.getStart().y / modelHeight;
-          g.fillRect(x, y + where, width, anchorHeight);
-        }
-      }
-    });
-    return bar;
-  }
-  
   @Override
   public void dispose() {
   }

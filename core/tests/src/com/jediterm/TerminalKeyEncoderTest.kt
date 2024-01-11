e1a30ea9bb306f727a9f0df74c3960b17dfeaa8f
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-01-11 23:13:07 +0100

    convert `TerminalKeyEncoderTest` to Kotlin to access internal `Ascii` without red code

--- core/tests/src/com/jediterm/TerminalKeyEncoderTest.kt
+++ core/tests/src/com/jediterm/TerminalKeyEncoderTest.kt
@@ -1,51 +1,55 @@
-package com.jediterm;
+package com.jediterm
 
-import com.jediterm.core.Platform;
-import com.jediterm.core.input.InputEvent;
-import com.jediterm.core.input.KeyEvent;
-import com.jediterm.core.util.Ascii;
-import com.jediterm.terminal.TerminalKeyEncoder;
-import junit.framework.TestCase;
-import org.junit.Assert;
+import com.jediterm.core.Platform
+import com.jediterm.core.input.InputEvent
+import com.jediterm.core.input.KeyEvent
+import com.jediterm.core.util.Ascii
+import com.jediterm.terminal.TerminalKeyEncoder
+import org.junit.Assert
+import org.junit.Test
 
-/**
- * @author traff
- */
-public class TerminalKeyEncoderTest extends TestCase {
-  
-  public void testAltBackspace() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    Assert.assertArrayEquals(new byte[]{Ascii.ESC, Ascii.DEL}, terminalKeyEncoder.getCode('\b', InputEvent.ALT_MASK));
+class TerminalKeyEncoderTest {
+
+  @Test
+  fun `Alt Backspace`() {
+    assertKeyCode(Ascii.BS.toInt(), InputEvent.ALT_MASK, byteArrayOf(Ascii.ESC, Ascii.DEL))
   }
 
-  public void testAltLeft() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    byte[] expected = Platform.current() == Platform.Mac ? new byte[]{Ascii.ESC, 'b'} : new byte[]{Ascii.ESC, '[', '1', ';', '3', 'D'};
-    Assert.assertArrayEquals(expected, terminalKeyEncoder.getCode(KeyEvent.VK_LEFT, InputEvent.ALT_MASK));
+  @Test
+  fun `Alt Left`() {
+    val expected = if (Platform.isMac()) prependEsc("b") else prependEsc("[1;3D")
+    assertKeyCode(KeyEvent.VK_LEFT, InputEvent.ALT_MASK, expected)
   }
 
-  public void testShiftLeft() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    byte[] expected = new byte[]{Ascii.ESC, '[', '1', ';', '2', 'D'};
-    Assert.assertArrayEquals(expected, terminalKeyEncoder.getCode(KeyEvent.VK_LEFT, InputEvent.SHIFT_MASK));
+  @Test
+  fun `Shift Left`() {
+    assertKeyCode(KeyEvent.VK_LEFT, InputEvent.SHIFT_MASK, prependEsc("[1;2D"))
   }
 
-  public void testShiftLeftApplication() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    terminalKeyEncoder.arrowKeysApplicationSequences();
-    byte[] expected = new byte[]{Ascii.ESC, '[', '1', ';', '2', 'D'};
-    Assert.assertArrayEquals(expected, terminalKeyEncoder.getCode(KeyEvent.VK_LEFT, InputEvent.SHIFT_MASK));
+  @Test
+  fun `Shift Left application`() {
+    assertKeyCode(KeyEvent.VK_LEFT, InputEvent.SHIFT_MASK, prependEsc("[1;2D"))
   }
 
-  public void testControlF1() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    byte[] expected = new byte[]{Ascii.ESC, '[', '1', ';', '5', 'P'};
-    Assert.assertArrayEquals(expected, terminalKeyEncoder.getCode(KeyEvent.VK_F1, InputEvent.CTRL_MASK));
+  @Test
+  fun `Control F1`() {
+    assertKeyCode(KeyEvent.VK_F1, InputEvent.CTRL_MASK, prependEsc("[1;5P"))
   }
 
-  public void testControlF11() {
-    TerminalKeyEncoder terminalKeyEncoder = new TerminalKeyEncoder();
-    byte[] expected = new byte[]{Ascii.ESC, '[', '2', '3', ';', '5', '~'};
-    Assert.assertArrayEquals(expected, terminalKeyEncoder.getCode(KeyEvent.VK_F11, InputEvent.CTRL_MASK));
+  @Test
+  fun `Control F11`() {
+    assertKeyCode(KeyEvent.VK_F11, InputEvent.CTRL_MASK, prependEsc("[23;5~"))
   }
+
+  private fun assertKeyCode(key: Int, modifiers: Int, expectedKeyCodeStr: String) {
+    assertKeyCode(key, modifiers, expectedKeyCodeStr.toByteArray(Charsets.UTF_8))
+  }
+
+  private fun assertKeyCode(key: Int, modifiers: Int, expectedKeyCode: ByteArray) {
+    val keyEncoder = TerminalKeyEncoder()
+    val actual = keyEncoder.getCode(key, modifiers)
+    Assert.assertArrayEquals(expectedKeyCode, actual)
+  }
+
+  private fun prependEsc(str: String): String = Ascii.ESC_CHAR + str
 }

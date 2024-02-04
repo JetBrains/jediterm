c3b0041624a54d83db0a8631fb03ff8f890dc4eb
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-02-04 21:19:48 +0100

    present multiple spaces as `<S:count>`

--- JediTerm/src/main/java/com/jediterm/ui/debug/ControlSequenceVisualizer.kt
+++ JediTerm/src/main/java/com/jediterm/ui/debug/ControlSequenceVisualizer.kt
@@ -1,6 +1,8 @@
 package com.jediterm.ui.debug
 
 import com.jediterm.terminal.util.CharUtils
+import java.lang.IllegalStateException
+import java.util.regex.Pattern
 
 internal object ControlSequenceVisualizer {
   fun getVisualizedString(logStart: Int,
@@ -12,12 +14,39 @@ internal object ControlSequenceVisualizer {
       chunks = TeseqVisualizer().apply(chunks)
     }
     if (settings.showInvisibleCharacters) {
-      chunks = chunks.map { CharUtils.toHumanReadableText(it) }
+      chunks = chunks.map { CharUtils.toHumanReadableText(toHumanReadableSpace(it)) }
     }
     if (settings.showChunkId) {
       chunks = withChunkId(logStart, chunks, originalChunks)
     }
     return chunks.joinToString("")
+  }
+
+  private fun toHumanReadableSpace(escSeq: String): String = makeCharHumanReadable(escSeq, ' ', "S")
+
+  @Suppress("SameParameterValue")
+  private fun makeCharHumanReadable(escSeq: String, ch: Char, presentable: String): String {
+    val pattern = Pattern.compile(Pattern.quote(ch.toString()) + "+")
+    val matcher = pattern.matcher(escSeq)
+    val builder = StringBuilder()
+    var lastInd = 0
+    while (matcher.find()) {
+      val startInd = matcher.start()
+      val endInd = matcher.end()
+      val spaces = escSeq.substring(startInd, endInd)
+      if (spaces != ch.toString().repeat(endInd - startInd)) {
+        throw IllegalStateException("Not spaces")
+      }
+      builder.append(escSeq.substring(lastInd, startInd))
+      if (spaces.length > 1) {
+        builder.append("<$presentable:").append(spaces.length).append(">")
+      }
+      else {
+        builder.append("<$presentable>")
+      }
+      lastInd = endInd
+    }
+    return builder.toString()
   }
 
   private fun withChunkId(logStart: Int, chunks: List<String>, originalChunks: List<String>): List<String> {

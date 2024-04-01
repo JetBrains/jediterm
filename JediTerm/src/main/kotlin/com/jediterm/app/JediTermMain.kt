JetBrains/jediterm

commit 32d48320121932d1d850279d0ca31440589e9579

convert `com.jediterm.core.Platform` to Kotlin to make it `internal`

JediTerm/src/main/kotlin/com/jediterm/app/JediTermMain.kt
@@ -1,6 +1,5 @@
 package com.jediterm.app
 
-import com.jediterm.core.Platform
 import com.jediterm.pty.PtyProcessTtyConnector
 import com.jediterm.terminal.LoggingTtyConnector
 import com.jediterm.terminal.LoggingTtyConnector.TerminalState
@@ -63,12 +62,12 @@ class JediTerm : AbstractTerminalFrame() {
   override fun createTtyConnector(): TtyConnector {
     try {
       val envs: Map<String, String> = configureEnvironmentVariables()
-      val command: Array<String> = if (Platform.current() == Platform.Windows) {
+      val command: Array<String> = if (isWindows()) {
         arrayOf("powershell.exe")
       }
       else {
         val shell = envs["SHELL"] ?: "/bin/bash"
-        if (Platform.current() == Platform.Mac) arrayOf(shell, "--login") else arrayOf(shell)
+        if (isMacOS()) arrayOf(shell, "--login") else arrayOf(shell)
       }
       val workingDirectory = Path.of(".").toAbsolutePath().normalize().pathString
 
@@ -93,10 +92,10 @@ class JediTerm : AbstractTerminalFrame() {
 
   private fun configureEnvironmentVariables(): Map<String, String> {
     val envs = HashMap(System.getenv())
-    if (Platform.current() == Platform.Mac) {
+    if (isMacOS()) {
       envs["LC_CTYPE"] = Charsets.UTF_8.name()
     }
-    if (Platform.current() != Platform.Windows) {
+    if (isWindows()) {
       envs["TERM"] = "xterm-256color"
     }
     return envs

JediTerm/src/main/java/com/jediterm/app/platformUtil.kt
@@ -1,0 +1,9 @@
+package com.jediterm.app
+
+internal fun isWindows(): Boolean {
+  return System.getProperty("os.name").lowercase().startsWith("windows")
+}
+
+internal fun isMacOS(): Boolean {
+  return System.getProperty("os.name").lowercase().startsWith("mac")
+}

JediTerm/src/main/java/com/jediterm/terminal/ui/settings/DefaultTabbedSettingsProvider.java
@@ -1,6 +1,5 @@
 package com.jediterm.terminal.ui.settings;
 
-import com.jediterm.core.Platform;
 import com.jediterm.terminal.TtyConnector;
 import com.jediterm.terminal.ui.TerminalActionPresentation;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+
+import static com.jediterm.app.PlatformUtilKt.isMacOS;
 
 /**
  * @author traff
@@ -24,28 +25,28 @@ public class DefaultTabbedSettingsProvider extends DefaultSettingsProvider imple
   }
   @Override
   public @NotNull TerminalActionPresentation getNewSessionActionPresentation() {
-    return new TerminalActionPresentation("New Session", Platform.isMac()
+    return new TerminalActionPresentation("New Session", isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
   }
 
   @Override
   public @NotNull TerminalActionPresentation getCloseSessionActionPresentation() {
-    return new TerminalActionPresentation("Close Session", Platform.isMac()
+    return new TerminalActionPresentation("Close Session", isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
   }
 
   @Override
   public @NotNull TerminalActionPresentation getPreviousTabActionPresentation() {
-    return new TerminalActionPresentation("Previous Tab", Platform.isMac()
+    return new TerminalActionPresentation("Previous Tab", isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, InputEvent.CTRL_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, InputEvent.ALT_DOWN_MASK));
   }
 
   @Override
   public @NotNull TerminalActionPresentation getNextTabActionPresentation() {
-    return new TerminalActionPresentation("Next Tab", Platform.isMac()
+    return new TerminalActionPresentation("Next Tab", isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, InputEvent.CTRL_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, InputEvent.ALT_DOWN_MASK));
   }

core/src/com/jediterm/core/Platform.kt
@@ -1,45 +1,31 @@
-package com.jediterm.core;
+package com.jediterm.core
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Locale;
-
-public enum Platform {
+internal enum class Platform {
   Windows,
-  OS2,
-  Mac,
+
+  @Suppress("EnumEntryName")
+  macOS,
   Linux,
   Other;
 
-  private static Platform cachedPlatform;
+  companion object {
+    private var CURRENT: Platform = detectCurrent()
 
-  public static @NotNull Platform current() {
-    Platform current = cachedPlatform;
-    if (current == null) {
-      current = detectCurrent();
-      cachedPlatform = current;
+    @JvmStatic
+    fun current(): Platform = CURRENT
+
+    @JvmStatic
+    fun isWindows(): Boolean = CURRENT == Windows
+
+    @JvmStatic
+    fun isMacOS(): Boolean = CURRENT == macOS
+
+    private fun detectCurrent(): Platform {
+      val osName = System.getProperty("os.name").lowercase()
+      if (osName.startsWith("windows")) return Windows
+      if (osName.startsWith("mac")) return macOS
+      if (osName.startsWith("linux")) return Linux
+      return Other
     }
-    return current;
-  }
-
-  public static boolean isWindows() {
-    return Platform.current() == Platform.Windows;
-  }
-
-  public static boolean isMac() {
-    return Platform.current() == Platform.Mac;
-  }
-
-  private static @NotNull Platform detectCurrent() {
-    String osName = getOsNameLowerCase();
-    if (osName.startsWith("windows")) return Platform.Windows;
-    if (osName.startsWith("os/2") || osName.startsWith("os2")) return Platform.OS2;
-    if (osName.startsWith("mac")) return Platform.Mac;
-    if (osName.startsWith("linux")) return Platform.Linux;
-    return Platform.Other;
-  }
-
-  private static @NotNull String getOsNameLowerCase() {
-    return System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
   }
 }

core/src/com/jediterm/terminal/TerminalKeyEncoder.java
@@ -75,7 +75,7 @@ public class TerminalKeyEncoder {
   }
 
   private void configureLeftRight() {
-    if (myPlatform == Platform.Mac) {
+    if (myPlatform == Platform.macOS) {
       putCode(new KeyCodeAndModifier(VK_RIGHT, InputEvent.ALT_MASK), ESC, 'f'); // ^[f
       putCode(new KeyCodeAndModifier(VK_LEFT, InputEvent.ALT_MASK), ESC, 'b'); // ^[b
     }

core/tests/src/com/jediterm/TerminalKeyEncoderTest.kt
@@ -17,7 +17,7 @@ class TerminalKeyEncoderTest {
 
   @Test
   fun `Alt Left`() {
-    val expected = if (Platform.isMac()) prependEsc("b") else prependEsc("[1;3D")
+    val expected = if (Platform.isMacOS()) prependEsc("b") else prependEsc("[1;3D")
     assertKeyCode(KeyEvent.VK_LEFT, InputEvent.ALT_MASK, expected)
   }
 

ui/src/com/jediterm/terminal/ui/settings/DefaultSettingsProvider.java
@@ -27,7 +27,7 @@ public class DefaultSettingsProvider implements SettingsProvider {
 
   @Override
   public @NotNull TerminalActionPresentation getCopyActionPresentation() {
-    KeyStroke keyStroke = Platform.isMac()
+    KeyStroke keyStroke = Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.META_DOWN_MASK)
       // CTRL + C is used for signal; use CTRL + SHIFT + C instead
       : KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
@@ -36,7 +36,7 @@ public class DefaultSettingsProvider implements SettingsProvider {
 
   @Override
   public @NotNull TerminalActionPresentation getPasteActionPresentation() {
-    KeyStroke keyStroke = Platform.isMac()
+    KeyStroke keyStroke = Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.META_DOWN_MASK)
       // CTRL + V is used for signal; use CTRL + SHIFT + V instead
       : KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
@@ -45,7 +45,7 @@ public class DefaultSettingsProvider implements SettingsProvider {
 
   @Override
   public @NotNull TerminalActionPresentation getClearBufferActionPresentation() {
-    return new TerminalActionPresentation("Clear Buffer", Platform.isMac()
+    return new TerminalActionPresentation("Clear Buffer", Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_K, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
   }
@@ -64,21 +64,21 @@ public class DefaultSettingsProvider implements SettingsProvider {
 
   @Override
   public @NotNull TerminalActionPresentation getLineUpActionPresentation() {
-    return new TerminalActionPresentation("Line Up", Platform.isMac()
+    return new TerminalActionPresentation("Line Up", Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_UP, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_UP, InputEvent.CTRL_DOWN_MASK));
   }
 
   @Override
   public @NotNull TerminalActionPresentation getLineDownActionPresentation() {
-    return new TerminalActionPresentation("Line Down", Platform.isMac()
+    return new TerminalActionPresentation("Line Down", Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, InputEvent.CTRL_DOWN_MASK));
   }
 
   @Override
   public @NotNull TerminalActionPresentation getFindActionPresentation() {
-    return new TerminalActionPresentation("Find", Platform.isMac()
+    return new TerminalActionPresentation("Find", Platform.isMacOS()
       ? KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.META_DOWN_MASK)
       : KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
   }
@@ -98,7 +98,7 @@ public class DefaultSettingsProvider implements SettingsProvider {
     String fontName;
     if (Platform.isWindows()) {
       fontName = "Consolas";
-    } else if (Platform.isMac()) {
+    } else if (Platform.isMacOS()) {
       fontName = "Menlo";
     } else {
       fontName = "Monospaced";

commit 23b361b1d4ebdde97bf400a23a4bdec98938f259

set `TERM` environment variable for Unix shells only
Regression after 32d48320121932d1d850279d0ca31440589e9579

JediTerm/src/main/kotlin/com/jediterm/app/JediTermMain.kt
@@ -95,7 +95,7 @@ class JediTerm : AbstractTerminalFrame() {
     if (isMacOS()) {
       envs["LC_CTYPE"] = Charsets.UTF_8.name()
     }
-    if (isWindows()) {
+    if (!isWindows()) {
       envs["TERM"] = "xterm-256color"
     }
     return envs
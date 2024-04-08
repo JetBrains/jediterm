JetBrains/jediterm

commit b33bf1ce59fab94bf84531d0a556ee37f73b58c3

drop style merging in favor of `StyleState.getBackground(TerminalColor)` / `StyleState.getForeground(TerminalColor)`

VERSION
@@ -1,1 +1,1 @@
-3.28
+3.29

core/src/com/jediterm/terminal/TextStyle.java
@@ -3,17 +3,13 @@ package com.jediterm.terminal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.ref.WeakReference;
 import java.util.EnumSet;
 import java.util.Objects;
-import java.util.WeakHashMap;
 
 public class TextStyle {
   private static final EnumSet<Option> NO_OPTIONS = EnumSet.noneOf(Option.class);
 
   public static final TextStyle EMPTY = new TextStyle();
-
-  private static final WeakHashMap<TextStyle, WeakReference<TextStyle>> styles = new WeakHashMap<>();
 
   private final TerminalColor myForeground;
   private final TerminalColor myBackground;
@@ -31,22 +27,6 @@ public class TextStyle {
     myForeground = foreground;
     myBackground = background;
     myOptions = options.clone();
-  }
-
-  @NotNull
-  public static TextStyle getCanonicalStyle(TextStyle currentStyle) {
-    if (currentStyle instanceof HyperlinkStyle) {
-      return currentStyle;
-    }
-    final WeakReference<TextStyle> canonRef = styles.get(currentStyle);
-    if (canonRef != null) {
-      final TextStyle canonStyle = canonRef.get();
-      if (canonStyle != null) {
-        return canonStyle;
-      }
-    }
-    styles.put(currentStyle, new WeakReference<>(currentStyle));
-    return currentStyle;
   }
 
   @Nullable
@@ -121,7 +101,7 @@ public class TextStyle {
   public static class Builder {
     private TerminalColor myForeground;
     private TerminalColor myBackground;
-    private EnumSet<Option> myOptions;
+    private final EnumSet<Option> myOptions;
 
     public Builder(@NotNull TextStyle textStyle) {
       myForeground = textStyle.myForeground;

core/src/com/jediterm/terminal/model/StyleState.java
@@ -5,33 +5,18 @@ import com.jediterm.terminal.TextStyle;
 import org.jetbrains.annotations.NotNull;
 
 public class StyleState {
-  private TextStyle myCurrentStyle = TextStyle.EMPTY;
-  private TextStyle myDefaultStyle = TextStyle.EMPTY;
-  
-  private TextStyle myMergedStyle = null;
+  private volatile TextStyle myCurrentStyle = TextStyle.EMPTY;
+  private volatile TextStyle myDefaultStyle = TextStyle.EMPTY;
 
   public StyleState() {
   }
 
-  public TextStyle getCurrent() {
-    return TextStyle.getCanonicalStyle(getMergedStyle());
-  }
-
-  @NotNull
-  private static TextStyle merge(@NotNull TextStyle style, @NotNull TextStyle defaultStyle) {
-    TextStyle.Builder builder = style.toBuilder();
-    if (style.getBackground() == null && defaultStyle.getBackground() != null) {
-      builder.setBackground(defaultStyle.getBackground());
-    }
-    if (style.getForeground() == null && defaultStyle.getForeground() != null) {
-      builder.setForeground(defaultStyle.getForeground());
-    }
-    return builder.build();
+  public @NotNull TextStyle getCurrent() {
+    return myCurrentStyle;
   }
 
   public void reset() {
     myCurrentStyle = myDefaultStyle;
-    myMergedStyle = null;
   }
 
   public void set(StyleState styleState) {
@@ -40,7 +25,6 @@ public class StyleState {
 
   public void setDefaultStyle(TextStyle defaultStyle) {
     myDefaultStyle = defaultStyle;
-    myMergedStyle = null;
   }
 
   public TerminalColor getBackground() {
@@ -61,13 +45,5 @@ public class StyleState {
 
   public void setCurrent(TextStyle current) {
     myCurrentStyle = current;
-    myMergedStyle = null;
-  }
-
-  private TextStyle getMergedStyle() {
-    if (myMergedStyle == null) {
-      myMergedStyle = merge(myCurrentStyle, myDefaultStyle);
-    }
-    return myMergedStyle;
   }
 }

commit f9461674d8975ab478ccb3e862274915f48a5206

restore support for inverted style (IDEA-350132)
https://youtrack.jetbrains.com/issue/IDEA-350132/Terminal-reverse-video-effect-is-broken
Regression after b33bf1ce59fab94bf84531d0a556ee37f73b58c3

VERSION
@@ -1,1 +1,1 @@
-3.41
+3.42

core/src/com/jediterm/terminal/TextStyle.java
@@ -62,14 +62,6 @@ public class TextStyle {
     return Objects.hash(myForeground, myBackground, myOptions);
   }
 
-  public TerminalColor getBackgroundForRun() {
-    return myOptions.contains(Option.INVERSE) ? myForeground : myBackground;
-  }
-
-  public TerminalColor getForegroundForRun() {
-    return myOptions.contains(Option.INVERSE) ? myBackground : myForeground;
-  }
-
   @NotNull
   public Builder toBuilder() {
     return new Builder(this);

ui/src/com/jediterm/terminal/ui/TerminalPanel.java
@@ -754,20 +754,12 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
 
   @Override
   public @NotNull java.awt.Color getBackground() {
-    return getPaletteBackground(myStyleState.getBackground());
-  }
-
-  private @NotNull java.awt.Color getPaletteBackground(@NotNull TerminalColor color) {
-    return AwtTransformers.toAwtColor(getPalette().getBackground(color));
+    return AwtTransformers.toAwtColor(getWindowBackground());
   }
 
   @Override
   public @NotNull java.awt.Color getForeground() {
-    return getPaletteForeground(myStyleState.getForeground());
-  }
-
-  private @NotNull java.awt.Color getPaletteForeground(@NotNull TerminalColor color) {
-    return AwtTransformers.toAwtColor(getPalette().getForeground(color));
+    return AwtTransformers.toAwtColor(getWindowForeground());
   }
 
   @Override
@@ -986,14 +978,47 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
   }
 
   @Override
-  public @Nullable Color getWindowForeground() {
-    return AwtTransformers.fromAwtColor(getForeground());
+  public @NotNull Color getWindowForeground() {
+    return toForeground(mySettingsProvider.getDefaultForeground());
   }
 
   @Override
-  public @Nullable Color getWindowBackground() {
-    // Return RGB color because we don't have palette information outside TerminalPanel.
-    return AwtTransformers.fromAwtColor(getBackground());
+  public @NotNull Color getWindowBackground() {
+    return toBackground(mySettingsProvider.getDefaultBackground());
+  }
+
+  private @NotNull java.awt.Color getEffectiveForeground(@NotNull TextStyle style) {
+    Color color = style.hasOption(Option.INVERSE) ? getBackground(style) : getForeground(style);
+    return AwtTransformers.toAwtColor(color);
+  }
+
+  private @NotNull java.awt.Color getEffectiveBackground(@NotNull TextStyle style) {
+    Color color = style.hasOption(Option.INVERSE) ? getForeground(style) : getBackground(style);
+    return AwtTransformers.toAwtColor(color);
+  }
+
+  private @NotNull Color getForeground(@NotNull TextStyle style) {
+    TerminalColor foreground = style.getForeground();
+    return foreground != null ? toForeground(foreground) : getWindowForeground();
+  }
+
+  private @NotNull Color toForeground(@NotNull TerminalColor terminalColor) {
+    if (terminalColor.isIndexed()) {
+      return getPalette().getForeground(terminalColor);
+    }
+    return terminalColor.toColor();
+  }
+
+  private @NotNull Color getBackground(@NotNull TextStyle style) {
+    TerminalColor background = style.getBackground();
+    return background != null ? toBackground(background) : getWindowBackground();
+  }
+
+  private @NotNull Color toBackground(@NotNull TerminalColor terminalColor) {
+    if (terminalColor.isIndexed()) {
+      return getPalette().getBackground(terminalColor);
+    }
+    return terminalColor.toColor();
   }
 
   protected int getInsetX() {
@@ -1179,9 +1204,9 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
       int width = Math.min(textLength * TerminalPanel.this.myCharSize.width, TerminalPanel.this.getWidth() - xCoord);
       int lineStrokeSize = 2;
 
-      java.awt.Color fgColor = getPaletteForeground(myStyleState.getForeground(style.getForegroundForRun()));
+      java.awt.Color fgColor = getEffectiveForeground(style);
       TextStyle inversedStyle = getInversedStyle(style);
-      java.awt.Color inverseBg = getPaletteBackground(myStyleState.getBackground(inversedStyle.getBackgroundForRun()));
+      java.awt.Color inverseBg = getEffectiveBackground(inversedStyle);
 
       switch (myShape) {
         case BLINK_BLOCK:
@@ -1270,7 +1295,7 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
       }
     }
 
-    java.awt.Color backgroundColor = getPaletteBackground(myStyleState.getBackground(style.getBackgroundForRun()));
+    java.awt.Color backgroundColor = getEffectiveBackground(style);
     gfx.setColor(backgroundColor);
     gfx.fillRect(xCoord,
             yCoord,
@@ -1409,9 +1434,9 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
   }
 
   private @NotNull java.awt.Color getStyleForeground(@NotNull TextStyle style) {
-    java.awt.Color foreground = getPaletteForeground(myStyleState.getForeground(style.getForegroundForRun()));
+    java.awt.Color foreground = getEffectiveForeground(style);
     if (style.hasOption(Option.DIM)) {
-      java.awt.Color background = getPaletteBackground(myStyleState.getBackground(style.getBackgroundForRun()));
+      java.awt.Color background = getEffectiveBackground(style);
       foreground = new java.awt.Color((foreground.getRed() + background.getRed()) / 2,
                              (foreground.getGreen() + background.getGreen()) / 2,
                              (foreground.getBlue() + background.getBlue()) / 2,

ui/src/com/jediterm/terminal/ui/settings/UserSettingsProvider.java
@@ -1,12 +1,14 @@
 package com.jediterm.terminal.ui.settings;
 
 import com.jediterm.terminal.HyperlinkStyle;
+import com.jediterm.terminal.TerminalColor;
 import com.jediterm.terminal.TextStyle;
 import com.jediterm.terminal.emulator.ColorPalette;
 import com.jediterm.terminal.model.TerminalTypeAheadSettings;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.Objects;
 
 public interface UserSettingsProvider {
   ColorPalette getTerminalColorPalette();
@@ -30,7 +32,22 @@ public interface UserSettingsProvider {
     return true;
   }
 
-  TextStyle getDefaultStyle();
+  default @NotNull TerminalColor getDefaultForeground() {
+    return Objects.requireNonNull(getDefaultStyle().getForeground());
+  }
+
+  default @NotNull TerminalColor getDefaultBackground() {
+    return Objects.requireNonNull(getDefaultStyle().getBackground());
+  }
+
+  /**
+   * @deprecated override {@link UserSettingsProvider#getDefaultForeground()} and
+   * {@link UserSettingsProvider#getDefaultBackground()} instead
+   */
+  @Deprecated
+  default @NotNull TextStyle getDefaultStyle() {
+    return new TextStyle(TerminalColor.BLACK, TerminalColor.WHITE);
+  }
 
   @NotNull TextStyle getSelectionColor();
 
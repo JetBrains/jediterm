c13ceb9cade8b3f9e2099ca36d347d3b292d70cb
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-08-07 14:37:56 +0300

    IJPL-159160 Use more time effective cyclic buffer for storing terminal lines
    We no longer need to recreate the whole list for add/remove line from start/middle operations. So, now these operations perform much faster.

---
+++ core/src/com/jediterm/terminal/model/CyclicBufferLinesStorage.kt
@@ -0,0 +1,57 @@
+package com.jediterm.terminal.model
+
+/**
+ * @param maxCapacity maximum number of stored lines; -1 means no restriction
+ */
+internal class CyclicBufferLinesStorage(private val maxCapacity: Int) : LinesStorage {
+
+  private val lines: ArrayDeque<TerminalLine> = ArrayDeque()
+
+  private val isCapacityLimited: Boolean = maxCapacity >= 0
+
+  override val size: Int
+    get() = lines.size
+
+  /** O(1) */
+  override fun get(index: Int): TerminalLine = lines[index]
+
+  /** O(size) */
+  override fun indexOf(line: TerminalLine): Int = lines.indexOf(line)
+
+  /**
+   * Amortized 0(1).
+   * The worst case is when we need to extend the internal storage of the array deque.
+   */
+  override fun addToTop(line: TerminalLine) {
+    if (isCapacityLimited && lines.size == maxCapacity) {
+      return
+    }
+    lines.addFirst(line)
+  }
+
+  /**
+   * Amortized 0(1).
+   * The worst case is when we need to extend the internal storage of the array deque.
+   */
+  override fun addToBottom(line: TerminalLine) {
+    lines.addLast(line)
+    if (isCapacityLimited && lines.size > maxCapacity) {
+      lines.removeFirst()
+    }
+  }
+
+  /** O(1) */
+  override fun removeFromTop(): TerminalLine {
+    return lines.removeFirst()
+  }
+
+  /** O(1) */
+  override fun removeFromBottom(): TerminalLine {
+    return lines.removeLast()
+  }
+
+  /** O(size) */
+  override fun clear() = lines.clear()
+
+  override fun iterator(): Iterator<TerminalLine> = lines.iterator()
+}

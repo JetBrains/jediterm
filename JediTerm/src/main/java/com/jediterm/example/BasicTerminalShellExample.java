JetBrains/jediterm

commit 1eb50648860af583a2c7d64f0eb09c2bc7d5204d

restore default cursor on soft/hard resets

ui/src/com/jediterm/terminal/ui/TerminalPanel.java
@@ -42,6 +42,7 @@ import java.text.BreakIterator;
 import java.text.CharacterIterator;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -1099,11 +1100,10 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
     // cursor state
     private boolean myCursorIsShown; // blinking state
     private final Point myCursorCoordinates = new Point();
-    private CursorShape myShape = CursorShape.BLINK_BLOCK;
+    private @Nullable CursorShape myShape;
 
     // terminal modes
     private boolean myShouldDrawCursor = true;
-    private boolean myBlinking = true;
 
     private long myLastCursorChange;
     private boolean myCursorHasChanged;
@@ -1133,12 +1133,8 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
       myShouldDrawCursor = shouldDrawCursor;
     }
 
-    public void setBlinking(boolean blinking) {
-      myBlinking = blinking;
-    }
-
     public boolean isBlinking() {
-      return myBlinking && (getBlinkingPeriod() > 0);
+      return getEffectiveShape().isBlinking() && (getBlinkingPeriod() > 0);
     }
 
     public void cursorChanged() {
@@ -1208,7 +1204,7 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
       TextStyle inversedStyle = getInversedStyle(style);
       java.awt.Color inverseBg = getEffectiveBackground(inversedStyle);
 
-      switch (myShape) {
+      switch (getEffectiveShape()) {
         case BLINK_BLOCK:
         case STEADY_BLOCK:
           if (state == TerminalCursorState.SHOWING) {
@@ -1235,8 +1231,12 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
       }
     }
 
-    void setShape(CursorShape shape) {
-      this.myShape = shape;
+    void setShape(@Nullable CursorShape shape) {
+      myShape = shape;
+    }
+
+    @NotNull CursorShape getEffectiveShape() {
+      return Objects.requireNonNullElse(myShape, CursorShape.BLINK_BLOCK);
     }
   }
 
@@ -1544,20 +1544,8 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
   }
 
   @Override
-  public void setCursorShape(@NotNull CursorShape cursorShape) {
+  public void setCursorShape(@Nullable CursorShape cursorShape) {
     myCursor.setShape(cursorShape);
-    switch (cursorShape) {
-      case STEADY_BLOCK:
-      case STEADY_UNDERLINE:
-      case STEADY_VERTICAL_BAR:
-        myCursor.setBlinking(false);
-        break;
-      case BLINK_BLOCK:
-      case BLINK_UNDERLINE:
-      case BLINK_VERTICAL_BAR:
-        myCursor.setBlinking(true);
-        break;
-    }
   }
 
   public void beep() {

core/src/com/jediterm/terminal/CursorShape.java
@@ -1,11 +1,17 @@
 package com.jediterm.terminal;
 
-/** Current cursor shape as described by https://vt100.net/docs/vt510-rm/DECSCUSR.html. */
+/**
+ * Cursor shape as described by <a href="https://vt100.net/docs/vt510-rm/DECSCUSR.html">DECSCUSR</a>.
+ */
 public enum CursorShape {
-    BLINK_BLOCK,
-    STEADY_BLOCK,
-    BLINK_UNDERLINE,
-    STEADY_UNDERLINE,
-    BLINK_VERTICAL_BAR,
-    STEADY_VERTICAL_BAR
+  BLINK_BLOCK,
+  STEADY_BLOCK,
+  BLINK_UNDERLINE,
+  STEADY_UNDERLINE,
+  BLINK_VERTICAL_BAR,
+  STEADY_VERTICAL_BAR;
+
+  public boolean isBlinking() {
+    return this == BLINK_BLOCK || this == BLINK_UNDERLINE || this == BLINK_VERTICAL_BAR;
+  }
 }

core/src/com/jediterm/terminal/TerminalDisplay.java
@@ -11,7 +11,10 @@ import org.jetbrains.annotations.Nullable;
 public interface TerminalDisplay {
   void setCursor(int x, int y);
 
-  void setCursorShape(@NotNull CursorShape cursorShape);
+  /**
+   * Sets cursor shape, null means default.
+   */
+  void setCursorShape(@Nullable CursorShape cursorShape);
 
   void beep();
 

core/src/com/jediterm/terminal/model/JediTerminal.java
@@ -684,7 +684,7 @@ public class JediTerminal implements Terminal, TerminalMouseListener, TerminalCo
   }
 
   @Override
-  public void cursorShape(@NotNull CursorShape shape) {
+  public void cursorShape(@Nullable CursorShape shape) {
     myDisplay.setCursorShape(shape);
   }
 
@@ -844,7 +844,7 @@ public class JediTerminal implements Terminal, TerminalMouseListener, TerminalCo
     initMouseModes();
 
     cursorPosition(1, 1);
-    cursorShape(CursorShape.BLINK_BLOCK);
+    cursorShape(null);
   }
 
   private void initMouseModes() {

core/tests/src/com/jediterm/util/BackBufferDisplay.java
@@ -29,7 +29,7 @@ public class BackBufferDisplay implements TerminalDisplay {
   }
 
   @Override
-  public void setCursorShape(@NotNull CursorShape cursorShape) {
+  public void setCursorShape(@Nullable CursorShape cursorShape) {
   }
 
   @Override

commit 0ba19be18017da0becdf5515334e382c049a083b

provide ability to set default cursor in `TerminalPanel`
Regression after 1eb50648860af583a2c7d64f0eb09c2bc7d5204d. Now, `TerminalPanel.setCursorShape` passes `null` for the default cursor, but the full reset (`JediTerminal.reset`) is performed on startup, so a cursor configured in `JBTerminalPanel` constructor was cleared.

JediTerm/src/main/java/com/jediterm/example/BasicTerminalShellExample.java
@@ -1,6 +1,7 @@
 package com.jediterm.example;
 
 import com.jediterm.pty.PtyProcessTtyConnector;
+import com.jediterm.terminal.CursorShape;
 import com.jediterm.terminal.TtyConnector;
 import com.jediterm.terminal.ui.JediTermWidget;
 import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
@@ -21,6 +22,7 @@ public class BasicTerminalShellExample {
 
   private static @NotNull JediTermWidget createTerminalWidget() {
     JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
+    widget.getTerminalPanel().setDefaultCursorShape(CursorShape.BLINK_UNDERLINE);
     widget.setTtyConnector(createTtyConnector());
     widget.start();
     return widget;

JediTerm/src/main/java/com/jediterm/ui/AbstractTerminalFrame.java
@@ -3,6 +3,7 @@ package com.jediterm.ui;
 import com.jediterm.app.JediTerm;
 import com.jediterm.app.TtyConnectorWaitFor;
 import com.jediterm.core.compatibility.Point;
+import com.jediterm.terminal.CursorShape;
 import com.jediterm.terminal.Terminal;
 import com.jediterm.terminal.TtyConnector;
 import com.jediterm.terminal.model.SelectionUtil;
@@ -176,7 +177,9 @@ public abstract class AbstractTerminalFrame {
   }
 
   protected JediTermWidget createTerminalWidget(@NotNull SettingsProvider settingsProvider) {
-    return new JediTermWidget(settingsProvider);
+    JediTermWidget widget = new JediTermWidget(settingsProvider);
+    widget.getTerminalPanel().setDefaultCursorShape(CursorShape.BLINK_VERTICAL_BAR);
+    return widget;
   }
 
   private void sizeFrameForTerm(final JFrame frame) {

JediTerm/src/main/kotlin/com/jediterm/app/JediTermMain.kt
@@ -88,7 +88,7 @@ class JediTerm : AbstractTerminalFrame() {
   }
 
   override fun createTerminalWidget(settingsProvider: SettingsProvider): JediTermWidget {
-    val widget = JediTermWidget(settingsProvider)
+    val widget = super.createTerminalWidget(settingsProvider)
     widget.addHyperlinkFilter(UrlFilter())
     return widget
   }

VERSION
@@ -1,1 +1,1 @@
-3.45
+3.46

ui/src/com/jediterm/terminal/ui/TerminalPanel.java
@@ -1100,6 +1100,7 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
     // cursor state
     private boolean myCursorIsShown; // blinking state
     private final Point myCursorCoordinates = new Point();
+    private @NotNull CursorShape myDefaultCursorShape = CursorShape.BLINK_BLOCK;
     private @Nullable CursorShape myShape;
 
     // terminal modes
@@ -1236,7 +1237,11 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
     }
 
     @NotNull CursorShape getEffectiveShape() {
-      return Objects.requireNonNullElse(myShape, CursorShape.BLINK_BLOCK);
+      return Objects.requireNonNullElse(myShape, myDefaultCursorShape);
+    }
+
+    private void setDefaultShape(@NotNull CursorShape defaultShape) {
+      myDefaultCursorShape = defaultShape;
     }
   }
 
@@ -1546,6 +1551,10 @@ public class TerminalPanel extends JComponent implements TerminalDisplay, Termin
   @Override
   public void setCursorShape(@Nullable CursorShape cursorShape) {
     myCursor.setShape(cursorShape);
+  }
+
+  public void setDefaultCursorShape(@NotNull CursorShape defaultCursorShape) {
+    myCursor.setDefaultShape(defaultCursorShape);
   }
 
   public void beep() {
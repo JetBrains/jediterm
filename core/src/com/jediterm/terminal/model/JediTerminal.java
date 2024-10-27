45a599cd0ca5836bb80f1231149e6c734fabcb4b
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-10-27 20:05:30 +0100

    reduce visibility of `com.jediterm.terminal.model.TerminalTextBuffer#textProcessing`

--- core/src/com/jediterm/terminal/model/JediTerminal.java
+++ core/src/com/jediterm/terminal/model/JediTerminal.java
@@ -1047,7 +1047,7 @@ public class JediTerminal implements Terminal, TerminalMouseListener, TerminalCo
   @Override
   public void setLinkUriStarted(@NotNull String uri) {
     TextStyle style = myStyleState.getCurrent();
-    TextProcessing textProcessing = myTerminalTextBuffer.getTextProcessing();
+    TextProcessing textProcessing = myTerminalTextBuffer.getTextProcessing$core();
     if (textProcessing != null) {
       List<LinkResultItem> linkResultItems = textProcessing.applyFilter(uri);
       linkResultItems.stream()
